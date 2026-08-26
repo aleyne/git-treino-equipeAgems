0e7927a77917f9bf372c2ceff9e8befafcd7166d
aleyne <aleyne29@gmail.com>
2026-08-26 19:30:46 -0400

    Add files via upload

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+  def __init__(self, name, music):
+    self.name = name
+    self.music = music
+
+p1 = Person("Alexander", "Nothing Else Matters")
+p2 = Person("Karla", "Trem Azul")
+
+print(p1.name)
+print(p1.music)
+print(p2.name)
+print(p2.music)
